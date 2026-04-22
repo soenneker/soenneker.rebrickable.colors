c02445836bc399bdaed1305825e1209d0817de95
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 20:27:05 -0500

    TUnit

--- test/Soenneker.Rebrickable.Colors.Tests/RebrickableColorsUtilTests.cs
+++ test/Soenneker.Rebrickable.Colors.Tests/RebrickableColorsUtilTests.cs
@@ -4,7 +4,6 @@ using Soenneker.Rebrickable.Colors.Abstract;
 using Soenneker.Tests.HostedUnit;
 using System.IO;
 using System.Threading.Tasks;
-using Soenneker.Facts.Manual;
 
 namespace Soenneker.Rebrickable.Colors.Tests;
 
@@ -23,7 +22,7 @@ public sealed class RebrickableColorsUtilTests : HostedUnitTest
     {
     }
 
-    [ManualFact]
+    [Skip("Manual")]
     //[LocalOnly]
     public async ValueTask GetColors()
     {
